3790b04c36b53276700387fe6aac40509a2a6fe2
AJ de Leon <ajdeleon1064@gmail.com>
2018-03-27 00:07:38 -0400

    format footer

--- components/Footer.js
+++ components/Footer.js
@@ -3,10 +3,10 @@ import React from 'react'
 const Footer = () => (
   <div className="footer">
     <ul>
-      <li>Home</li>
-      <li>About</li>
-      <li>News</li>
-      <li>Contact</li>
+      <li><a href="#">Home</a></li>
+      <li><a href="#">About</a></li>
+      <li><a href="#">News</a></li>
+      <li><a href="#">Contact</a></li>
     </ul>
   </div>
 )
