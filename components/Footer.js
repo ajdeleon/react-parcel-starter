2ba7fdb3f41c2e99c0e49484476f158d09b321e5
AJ de Leon <ajdeleon1064@gmail.com>
2018-03-26 23:58:36 -0400

    add sticky footer

---
+++ components/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Footer = () => (
+  <div className="footer">
+    <ul>
+      <li>Home</li>
+      <li>About</li>
+      <li>News</li>
+      <li>Contact</li>
+    </ul>
+  </div>
+)
+
+export default Footer
--- index.js
+++ index.js
@@ -2,15 +2,20 @@ import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
 import './styles.scss'
 import InfoCardList from './components/InfoCardList';
+import Footer from './components/Footer'
 
 class App extends Component {
   render() {
     return (
-
     <div className="container">
-      <div className="main">Hello Parcel!</div>
-      <div className="content">Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic ad incidunt deserunt ex, mollitia molestiae placeat nihil eveniet nostrum beatae enim neque natus autem repudiandae sequi distinctio quam amet ipsam.</div>
-      <InfoCardList />
+      <div className="bodyContainer">
+        <div className="main">Hello Parcel!</div>
+        <div className="content">Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic ad incidunt deserunt ex, mollitia molestiae placeat nihil eveniet nostrum beatae enim neque natus autem repudiandae sequi distinctio quam amet ipsam.</div>
+        <InfoCardList />
+      </div>
+      <div className="footerContainer">
+        <Footer />
+      </div>
     </div>
     )
   }
