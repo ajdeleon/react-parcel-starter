67356c734ad92e3dc18e8f8025ba2b216f59b1ed
AJ de Leon <ajdeleon1064@gmail.com>
2018-04-11 23:30:13 -0400

    update

--- index.js
+++ index.js
@@ -10,7 +10,10 @@ class App extends Component {
     <div className="container">
       <div className="bodyContainer">
         <div className="main">Hello Parcel!</div>
-        <div className="content">Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic ad incidunt deserunt ex, mollitia molestiae placeat nihil eveniet nostrum beatae enim neque natus autem repudiandae sequi distinctio quam amet ipsam.</div>
+	<div className="content">Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic ad incidunt deserunt ex, mollitia molestiae placeat nihil eveniet nostrum beatae enim neque natus autem repudiandae sequi distinctio quam amet ipsam.</div>
+	<div className="testing">
+	  <p>Hey check this out!!</p>
+	</div>
         <InfoCardList />
       </div>
       <div className="footerContainer">
